d426b2fa7ae66403bc037e4680bef4831a08addc
Yesid <86808028+yesidexe@users.noreply.github.com>
2024-07-23 20:13:17 -0500

    Update import_sql.py

--- import_sql.py
+++ import_sql.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 
 pokemons = pd.read_csv('data/pokemons.csv')
 
-# con postgres
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/pokemons_database')
 
 pokemons.to_sql('pokemons', engine, index=False, if_exists='replace')
